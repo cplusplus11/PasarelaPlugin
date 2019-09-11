f6df9659ffb392e55f00d03d0f1cdb66d790e6af
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-09-11 16:21:55 -0500

    Update SdkPayme.js

--- www/SdkPayme.js
+++ www/SdkPayme.js
@@ -1,26 +1,7 @@
-//var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-//exports.coolMethod = function (arg0, success, error) {
-//    exec(success, error, 'SdkPayme', 'coolMethod', [arg0]);
-//};
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'SdkPayme', 'coolMethod', [arg0]);
+};
 
   
-// Constructor vacío
-function SdkPayme() {}
-
-// recibir los datos
-SdkPayme.prototype.coolMethod = function(message, successCallback, errorCallback) {
-  var options = {};
-  options.message = message;
-  cordova.exec(successCallback, errorCallback, 'SdkPayme', 'coolMethod', [options]);
-}
-
- // Instalar la variable en el window
-SdkPayme.install = function() {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-  window.plugins.sdkPayme = new SdkPayme();
-  return window.plugins.sdkPayme;
-};
-cordova.addConstructor(SdkPayme.install);
